a905cd2e34c14b7db96eda5ca4fa88312f3c3188
tuananvan <80084018+tuananvan@users.noreply.github.com>
2021-10-13 19:29:30 +0700

    BaiTapVeMang

---
+++ BaiTapMang/src/main/java/Bai2.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        Bai2.getRandValues(a);
+        float avg = Bai2.Average(a);
+        System.out.print("Average of above Array: " + avg);
+    }
+   
+    public static void getRandValues(int[] a) {
+        System.out.println("Enter values: ");
+        for (int i = 0; i < a.length; i++) {
+            Random rand = new Random();
+            a[i] = rand.nextInt(100)+1;
+        }
+        System.out.println("Array: ");
+        for(int i = 0; i < a.length; i++)               
+        System.out.println(a[i]);
+    }
+    
+    public static float Average(int[] a) {
+        float avg = 0;
+        for (int i = 0; i < a.length; i++) {
+            avg += a[i];
+        }
+        avg /= (float)(a.length);
+        return avg;
+    }
+}
